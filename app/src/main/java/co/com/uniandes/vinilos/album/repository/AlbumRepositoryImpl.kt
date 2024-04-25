cc9ba26845f0725bec44aaf0b2c847c601c0fa6f
Pedro Buitrago <p.buitragom@uniandes.edu.co>
2024-04-25 15:04:45 -0500

    :lipstick: HU-001: Cleaning-up

--- app/src/main/java/co/com/uniandes/vinilos/album/repository/AlbumRepositoryImpl.kt
+++ app/src/main/java/co/com/uniandes/vinilos/album/repository/AlbumRepositoryImpl.kt
@@ -12,25 +12,13 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class AlbumRepositoryImpl(private val context: Context) : AlbumRepository {
+
     private val serviceAdapter = AlbumServiceAdapter(context)
-    private var albums: List<Album> = emptyList()
     val albumsLiveData = MutableLiveData<List<Album>>()
 
     override fun getAlbum(): LiveData<Album> {
         val liveData = MutableLiveData<Album>()
-
-        /*val request = serviceAdapter.getAlbum(
-            Response.Listener { response ->
-                val gson = Gson()
-                val album = gson.fromJson(response, Album::class.java)
-                liveData.postValue(album)
-            },
-            Response.ErrorListener { error ->
-                // Handle error
-            }
-        )
-        serviceAdapter.instance.add(request)*/
-        //return liveData
+        //Po implementar
         return liveData
     }
 
