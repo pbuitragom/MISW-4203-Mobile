92b77957b0fe6a9cb91c0cc4de7acf0b00b2fd0a
Camilo Lesmes <c.lesmesl@uniandes.edu.co>
2024-04-28 16:30:47 -0500

    Tests unitarios de modelo

---
+++ app/src/test/java/co/com/uniandes/vinilos/model/AlbumTest.kt
@@ -0,0 +1,48 @@
+package co.com.uniandes.vinilos.album.model
+
+import com.google.gson.Gson
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class AlbumTest {
+
+    @Test
+    fun albumInitialization_isCorrect() {
+        val album = Album(
+            albumId = 1,
+            name = "Example Album",
+            cover = "example_cover.jpg",
+            releaseDate = "2024-04-28",
+            description = "This is an example album.",
+            genre = "Rock",
+            recordLabel = "Example Records"
+        )
+
+        assertEquals(1, album.albumId)
+        assertEquals("Example Album", album.name)
+        assertEquals("example_cover.jpg", album.cover)
+        assertEquals("2024-04-28", album.releaseDate)
+        assertEquals("This is an example album.", album.description)
+        assertEquals("Rock", album.genre)
+        assertEquals("Example Records", album.recordLabel)
+    }
+
+    @Test
+    fun albumSerialization_isCorrect() {
+        val album = Album(
+            albumId = 1,
+            name = "Example Album",
+            cover = "example_cover.jpg",
+            releaseDate = "2024-04-28",
+            description = "This is an example album.",
+            genre = "Rock",
+            recordLabel = "Example Records"
+        )
+
+        val gson = Gson()
+        val jsonString = gson.toJson(album)
+        val deserializedAlbum = gson.fromJson(jsonString, Album::class.java)
+
+        assertEquals(album, deserializedAlbum)
+    }
+}
---
+++ app/src/test/java/co/com/uniandes/vinilos/repository/AlbumRepositoryImplTest.kt
@@ -0,0 +1,4 @@
+package co.com.uniandes.vinilos.repository
+
+class AlbumRepositoryImplTest {
+}
